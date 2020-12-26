d3ab675145026cd5bcc2dd349c4e73cef8d89952
meganemura <meganemura@users.noreply.github.com>
2020-12-26 15:12:35 +0900

    Fix indentation

--- lib/rowdb/adapters/sync.rb
+++ lib/rowdb/adapters/sync.rb
@@ -5,58 +5,58 @@ require_relative 'adapter.rb'
 # Synchronous file system.
 ##
 class Rowdb
-class Sync < Adapter
+  class Sync < Adapter
 
-  ##
-  # Load a JSON string from a file.
-  #
-  # @return Hash data
-  ##
-  def read()
+    ##
+    # Load a JSON string from a file.
+    #
+    # @return Hash data
+    ##
+    def read()
 
-    json = nil
+      json = nil
 
-    # Load JSON inside a Javascript variable.
-    if @format == :js && File.exist?(@source)
-      File.open(@source, 'r') do |file|
-        json = file.read
-        # Fix double encoding issue due to JSON string becoming Ruby string.
-        json.gsub!('\\"', '"')
-        unwrap(json)
+      # Load JSON inside a Javascript variable.
+      if @format == :js && File.exist?(@source)
+        File.open(@source, 'r') do |file|
+          json = file.read
+          # Fix double encoding issue due to JSON string becoming Ruby string.
+          json.gsub!('\\"', '"')
+          unwrap(json)
+        end
+      # Load JSON string.
+      else
+        json = Oj.load_file(@source)
       end
-    # Load JSON string.
-    else
-      json = Oj.load_file(@source)
+
+      unless json.nil?
+
+        # Parse JSON.
+        data = Oj.load(json)
+        return data.transform_keys(&:to_sym)
+
+      end
+
+      return nil
+
     end
 
-    unless json.nil?
+    ##
+    # Save a Hash to a file as a JSON string or JS.
+    #
+    # @param Hash data
+    ##
+    def write(data)
 
-      # Parse JSON.
-      data = Oj.load(json)
-      return data.transform_keys(&:to_sym)
+      json = Oj.dump(data, mode: :compat)
 
-    end
+      Oj.to_file(@source, json)
 
-    return nil
+      if @format == :js
+        wrap()
+      end
 
-  end
-
-  ##
-  # Save a Hash to a file as a JSON string or JS.
-  #
-  # @param Hash data
-  ##
-  def write(data)
-
-    json = Oj.dump(data, mode: :compat)
-
-    Oj.to_file(@source, json)
-
-    if @format == :js
-      wrap()
     end
 
   end
-
-end
 end
